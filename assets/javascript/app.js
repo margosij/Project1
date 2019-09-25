999e3c08e2f17518c45baabf48812fa69a518c08
South Matic <Southmatic729@gmail.com>
2019-09-24 23:22:50 -0400

    added icanhazjoke api functionality

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,12 +1,42 @@
-var randomJoke;
-var jokeCategory = “Miscellaneous”;
-var format = “json”;
-var blacklistFlags = “nsfw”;
+//variable to store joke input
+var jokeCategory = "";
+//query variable
+var queryURL = "https://icanhazdadjoke.com/search?term=";
 
-$.ajax({
-    method: “GET”,
-    url: `https://sv443.net/jokeapi/category/${jokeCategory}?blacklistFlags=${blacklistFlags}&format=${format}`,
-    success: (data, status) => {
-        randomJoke = JSON.parse(data);
-    }
+console.log(queryURL);
+//when submit button is clicked
+$("#jokeInput").on("click", function(event) {
+//prevent default submit  
+ event.preventDefault();
+ //get value from input field
+  jokeCategory = $("#category")
+    .val()
+    .trim();
+  console.log(jokeCategory);
+  //if input field is empty, return
+  if (jokeCategory === undefined || jokeCategory.length == 0) {
+    return;
+  } else {
+      //build query variable with subject
+    queryURL = queryURL + jokeCategory;
+    $.ajax({
+        //headers syntax
+      headers: {
+        Accept: "application/json"
+      },
+      url: queryURL,
+      method: "GET"
+    }).then(function(randomJoke) {
+      console.log(randomJoke);
+      //set response array 
+      var jokes = randomJoke.results;
+      console.log(jokes);
+      //append jokes to display
+      for (i = 0; i < jokes.length; i++) {
+        var newJoke = $("<p>");
+        newJoke.text(jokes[i].joke);
+        $("#jokeGOHERE").append(newJoke);
+      }
+    });
+  }
 });
