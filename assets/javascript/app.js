4d270c078fb29e34fd0ced15f8d77de8a38b5fa9
Jake Campbell <jakecmail@gmail.com>
2019-09-27 09:58:57 -0400

    updating html/css to acommodate jokeAPI and YodaTranslatorAPI being integrated

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -83,9 +83,9 @@ $(document).ready(function() {
     for (var i = 0; i < jokesFromResponse.length; i++) {
       $(".main").tiltedpage_scroll({
         sectionContainer: "> section", // In case you don't want to use <section> tag, you can define your won CSS selector here
-        angle: 50, // You can define the angle of the tilted section here. Change this to false if you want to disable the tilted effect. The default value is 50 degrees.
+        angle: 25, // You can define the angle of the tilted section here. Change this to false if you want to disable the tilted effect. The default value is 50 degrees.
         opacity: true, // You can toggle the opacity effect with this option. The default value is true
-        scale: true, // You can toggle the scaling effect here as well. The default value is true.
+        scale: false, // You can toggle the scaling effect here as well. The default value is true.
         outAnimation: true // In case you do not want the out animation, you can toggle this to false. The defaul value is true.
       });
     }
